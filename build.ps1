915353491238e0852eb18b8620397fb979f8336d
Dave Moor <katterfelto@outlook.com>
2025-01-08 15:56:58 +0000

    Added build script

---
+++ build.ps1
@@ -0,0 +1,2 @@
+dotnet publish .\hosting\Windows\Garnet.worker\Garnet.worker.csproj  /p:PublishProfile=.\hosting\Windows\Garnet.worker\Properties\PublishProfiles\FolderProfile.pubxml
+exit $LASTEXITCODE;
